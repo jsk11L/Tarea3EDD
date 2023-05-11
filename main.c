7a5b4ac267e4aab1b650436b598cf6d1a83e5abb
jsk11L <127500553+jsk11L@users.noreply.github.com>
2023-05-11 02:13:40 +0000

    Menú hecho y dejé las funciones vacías

--- main.c
+++ main.c
@@ -1,7 +1,72 @@
+#include "Map.h"
+#include "list.h"
+#include "stack.h"
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World\n");
+void agregarTarea(grafo){}
+
+void establecerPrecedencia(grafo){}
+
+void mostrarTareas(grafo){}
+
+void marcarTareaCompletada(grafo){}
+
+void deshacerAccion(grafo){}
+
+void cargarArchivo(grafo){}
+
+int main() {
+  //Se inicializa el mapa de los jugadores y la lista disfrazada de pila.
+  Map* grafo = createMap(is_equal_string);
+  int opcion, titulo = 0;
+
+  while(1) {
+    if (titulo == 0) {
+      printf("=========================================\n");
+      printf(" === BIENVENIDO AL MENÚ DE TAREAS === \n");
+      titulo++;
+    }
+
+    printf("=========================================\n");
+    printf("Escoja una opción: \n\n");
+    printf("1. Agregar tarea\n");
+    printf("2. Establecer precedencia entre tareas\n");
+    printf("3. Mostrar tareas por hacer\n");
+    printf("4. Marcar tarea como completada\n");
+    printf("5. Deshacer última acción\n");
+    printf("6. Cargar datos de tareas desde un archivo de texto\n");
+    printf("7. Salir del programa\n");
+    
+    scanf("%d",&opcion);
+    while(opcion < 1 || opcion > 10) {
+      scanf("%d",&opcion);
+    }
+
+    switch(opcion) {
+      case 1:
+        agregarTarea(grafo);
+        break;
+      case 2:
+        establecerPrecedencia(grafo);
+        break;
+      case 3:
+        mostrarTareas(grafo);
+        break;
+      case 4:
+        marcarTareaCompletada(grafo);
+        break;
+      case 5:
+        deshacerAccion(grafo);
+        break;
+      case 6:
+        cargarArchivo(grafo);
+        break;
+      case 7:
+        printf("Cerrando el programa...\n");
+      return 0;
+    }
+  }
   return 0;
-  printf("Cono");
 }
